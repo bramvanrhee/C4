24462532cd85c027999e3fa5c5c151793038c360
Bram van Rhee <bramvanrhee01@gmail.com>
2020-05-26 14:23:24 +0200

    Controller toevoeging orderByMe

--- Inleveropdracht b/main.js
+++ Inleveropdracht b/main.js
@@ -5,5 +5,10 @@ app.controller('autoCtrl', function($scope) {
     {kenteken:'04-vt-23',kleur:'Geel',merk:'mercedes',aantaldeuren:'4',prijs:'32000'},
     {kenteken:'06-fd-tt',kleur:'grijs',merk:'volkswagen',aantaldeuren:'3',prijs:'21000'},
     {kenteken:'32-vc-aa',kleur:'zwart',merk:'kia',aantaldeuren:'3',prijs:'12000'},
-    {kenteken:'10-da-45',kleur:'rood',merk:'ferrari',aantaldeuren:'2',prijs:'100500'}];
+    {kenteken:'10-da-45',kleur:'rood',merk:'ferrari',aantaldeuren:'2',prijs:'100500'}
+    ];
+
+    $scope.orderByMe = function(auto) {
+        $scope.myOrderBy = auto;
+    }
 });
